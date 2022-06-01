f09f008eec73db0a69ffe4b19b0b3722e83a3c00
baagadaankaa <bogdan.sofroniy@gmail.com>
2022-06-01 13:06:08 +0200

    Bogdan : [add] initial project

---
+++ Source/TestTaskToFS/TestTaskToFS.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TestTaskToFS.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TestTaskToFS, "TestTaskToFS" );
